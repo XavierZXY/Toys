76320261d34a587dbd703f89bba92b6e5a2f0a52
XavierZXY <zxy_xavier@163.com>
2023-10-09 13:48:26 +0800

    feat: :construction: sharing status

--- rust-redis/src/main.rs
+++ rust-redis/src/main.rs
@@ -1,29 +1,33 @@
-use std::{os::unix::process, hash::Hash};
-
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     // Bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    println!("Listening");
+    let db = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
         // A new task is spawned for each inbound socket. The socket is
         // moved to the new task and processed there.
+        let db = db.clone();
+        println!("Accepted");
+
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    // A hashmap is used to store data
-    let mut db = HashMap::new();
 
     // Connection, provided by `mini-redis`, handles parsing frames from
     let mut connection = Connection::new(socket);
@@ -32,13 +36,13 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                // The value is stored as `Vec<u8>`
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
-            }
+            },
 
             Get(cmd) => {
-                // `clone` the data to satisfy the elision.
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     // `Frame::Bulk` expects data to be `Bytes`. This
                     // type conversion is handled with `into()`.
@@ -46,8 +50,9 @@ async fn process(socket: TcpStream) {
                 } else {
                     Frame::Null
                 }
-            }
-            cmd => panic!("unimplemented {:?}", cmd),
+            },
+            
+            cmd => panic!("unimplemented {:?}", cmd)
         };
 
         // Write the response to the client
